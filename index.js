717312e9b996dc74b5a82277779b8fea6fc7ae90
Doug Jones <DSJR1284@GMAIL.COM>
2021-06-16 21:30:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+function findMatching(drivers, string){
+    return drivers.filter(newDriver => newDriver.toUpperCase()=== string.toUpperCase())
+}
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(driverName => driverName.toUpperCase()[0] === string.toUpperCase()[0])
+}
+
+function matchName(drivers, string){
+    return drivers.filter(names => names.name === string)
+}
+
+
+
+// let bigCities = cities.filter(city => city.population > 3000000);
+// console.log(bigCities);
